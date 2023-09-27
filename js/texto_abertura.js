8a9fc2dd78981fbf1607676a8126ef64460bafce
bloraine7 <bruloraineb@gmail.com>
2023-09-27 20:44:51 -0300

    Atualização 27/09/2023

---
+++ js/texto_abertura.js
@@ -0,0 +1,21 @@
+function inicio() {
+    const textoOriginal = "Planeta Alien";
+    alert(`Olá avalia`);
+    console.log(`Olá avalia no navegador`);
+    const elementoTexto = document.querySelector("p")
+    const audio = document.querySelector("audio")
+    const btn = document.querySelector("button")
+    const atrasoRapido = 5000 // Atraso em milissegundos (2 segundos)
+    const atrasoDevagar = 7000
+
+    setTimeout(function () {
+        btn.style.display = "none"
+        audio.play()
+    }, atrasoRapido);
+
+    setTimeout(function () {
+        elementoTexto.textContent = textoOriginal
+    }, atrasoDevagar);
+
+
+}
